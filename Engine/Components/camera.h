8e62dd6070cdce2f09d3f978947f4d94877d97f2
DerpyNewbie <contact@derpynewbie.dev>
2025-05-27 10:28:44 +0900

    Apply view matrix directly and add near far properties

--- Engine/Components/camera.h
+++ Engine/Components/camera.h
@@ -13,9 +13,11 @@ class Camera : public Component
     };
 
     kViewMode m_view_mode_ = kPerspective;
-    float m_field_of_view_ = 103;
+    float m_field_of_view_ = 70;
+    float m_near_plane_ = 0.1f;
+    float m_far_plane_ = 1000.0f;
     float m_ortho_size_ = 50;
-    Color m_background_color_ = Color(0xFF000000);
+    Color m_background_color_ = Color(0x1A1A1AFF);
 
     void ApplyCameraSettingToDxLib() const;
 
@@ -29,6 +31,8 @@ public:
            CEREAL_NVP(m_view_mode_),
            CEREAL_NVP(m_field_of_view_),
            CEREAL_NVP(m_ortho_size_),
+           CEREAL_NVP(m_near_plane_),
+           CEREAL_NVP(m_far_plane_),
            CEREAL_NVP(m_background_color_));
     }
 };
