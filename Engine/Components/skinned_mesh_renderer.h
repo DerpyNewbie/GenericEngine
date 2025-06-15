0370c841215ceffc682b8deced67a1697b74dd75
DerpyNewbie <contact@derpynewbie.dev>
2025-06-15 19:37:16 +0900

    Make SkinnedMeshRenderer inherit MeshRenderer for now
    as it doesn't have any functionality aside from having bone transforms

--- Engine/Components/skinned_mesh_renderer.h
+++ Engine/Components/skinned_mesh_renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "DxLib.h"
 #include "game_object.h"
+#include "mesh_renderer.h"
 #include "renderer.h"
 #include "Rendering/texture2d.h"
 #include "Rendering/mesh.h"
@@ -10,27 +11,20 @@
 
 namespace engine
 {
-class SkinnedMeshRenderer : public Renderer
+class SkinnedMeshRenderer : public MeshRenderer
 {
     bool m_draw_bones_ = false;
     Texture2D m_texture_;
 
 public:
-    std::shared_ptr<Mesh> shared_mesh;
     std::vector<std::weak_ptr<Transform>> transforms;
-    int vertex_buffer_handle = -1;
-    int index_buffer_handle = -1;
 
     void OnInspectorGui() override;
-
-    void OnDraw() override;
-
-    void ReconstructBuffers();
-
+    
     template <class Archive>
     void serialize(Archive &ar)
     {
-        ar(cereal::base_class<Component>(this), CEREAL_NVP(shared_mesh), CEREAL_NVP(transforms));
+        ar(cereal::base_class<MeshRenderer>(this), CEREAL_NVP(shared_mesh), CEREAL_NVP(transforms));
     }
 };
 }
