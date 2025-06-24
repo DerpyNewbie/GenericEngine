7d52c45a38b3a4045ffe53c91937513d18a14cb6
xiar5 <xiar5@>
2025-06-24 23:04:43 +0900

    Add line_renderer

---
+++ Engine/Components/line_renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "renderer.h"
+#include "Rendering/line_renderer.h"
+
+namespace engine
+{
+class line_renderer : public Renderer
+{
+    LineRenderer m_LineRenderer;
+public:
+    void SetVertices(std::vector<Vertex> vertices);
+    void SetIndices(std::vector<uint32_t> indices);
+
+    void OnInspectorGui() override;
+    
+    void OnDraw() override;
+
+};
+}
