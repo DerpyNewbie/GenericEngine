6abffae5fa6fcb93f04aaa0ea1c6b503d298e31d
xiar5 <xiar5@>
2025-06-27 16:14:21 +0900

    Make CreateBuffer,UpdateBuffer Function

--- Engine/Rendering/material_block.cpp
+++ Engine/Rendering/material_block.cpp
@@ -1,3 +1,22 @@
 #include "pch.h"
-
 #include "material_block.h"
+
+void engine::MaterialBlock::CreateBuffer()
+{
+    params_float_buffer.Initialize(params_float.size());
+    params_int_buffer.Initialize(params_int.size());
+    params_vec2_buffer.Initialize(params_vec2.size());
+    params_vec3_buffer.Initialize(params_vec3.size());
+    params_vec4_buffer.Initialize(params_vec4.size());
+    params_mat4_buffer.Initialize(params_mat4.size());
+}
+
+void engine::MaterialBlock::UpdateBuffer()
+{
+    params_float_buffer.Upload(GetValues(params_float));
+    params_int_buffer.Upload(GetValues(params_int));
+    params_vec2_buffer.Upload(GetValues(params_vec2));
+    params_vec3_buffer.Upload(GetValues(params_vec3));
+    params_vec4_buffer.Upload(GetValues(params_vec4));
+    params_mat4_buffer.Upload(GetValues(params_mat4));
+}
--- Engine/Rendering/material_block.h
+++ Engine/Rendering/material_block.h
@@ -1,9 +1,12 @@
 #pragma once
+#include <directx/d3dx12.h>
+#include <ranges>
 #include "object.h"
+#include "CabotEngine/Graphics/Texture2D.h"
+#include "Rendering/CabotEngine/Graphics/StructuredBuffer.h"
 
 namespace engine
 {
-class Texture2D;
 /// <summary>
 /// Shared shader parameters.
 /// </summary>
@@ -12,23 +15,42 @@ class Texture2D;
 /// </remarks>
 class MaterialBlock : public Object
 {
+    template <typename T>
+    std::vector<T> GetValues(const std::vector<std::pair<std::string, T>>& pairs);
+
 public:
-    std::unordered_map<std::string, float> params_float;
-    std::unordered_map<std::string, int> params_int;
-    std::unordered_map<std::string, bool> params_bool;
-    std::unordered_map<std::string, Vector2> params_vec2;
-    std::unordered_map<std::string, Vector3> params_vec3;
-    std::unordered_map<std::string, Vector4> params_vec4;
-    std::unordered_map<std::string, Matrix> params_mat4;
-    std::unordered_map<std::string, std::shared_ptr<Texture2D>> params_tex2d;
+    std::vector<std::pair<std::string, float>> params_float;
+    std::vector<std::pair<std::string, int>> params_int;
+    std::vector<std::pair<std::string, Vector2>> params_vec2;
+    std::vector<std::pair<std::string, Vector3>> params_vec3;
+    std::vector<std::pair<std::string, Vector4>> params_vec4;
+    std::vector<std::pair<std::string, Matrix>> params_mat4;
+    std::vector<std::pair<std::string, std::shared_ptr<Texture2D>>> params_tex2d;
+
+    StructuredBuffer<float> params_float_buffer;
+    StructuredBuffer<int> params_int_buffer;
+    StructuredBuffer<Vector2> params_vec2_buffer;
+    StructuredBuffer<Vector3> params_vec3_buffer;
+    StructuredBuffer<Vector4> params_vec4_buffer;
+    StructuredBuffer<Matrix> params_mat4_buffer;
+
+    void CreateBuffer();
+    void UpdateBuffer();
 
     template <class Archive>
     void serialize(Archive &ar)
     {
         ar(cereal::base_class<Object>(this),
-           CEREAL_NVP(params_float), CEREAL_NVP(params_int), CEREAL_NVP(params_bool),
+           CEREAL_NVP(params_float), CEREAL_NVP(params_int),
            CEREAL_NVP(params_vec2), CEREAL_NVP(params_vec3), CEREAL_NVP(params_vec4), CEREAL_NVP(params_mat4),
            CEREAL_NVP(params_tex2d));
     }
 };
+
+template <typename T>
+std::vector<T> MaterialBlock::GetValues(const std::vector<std::pair<std::string, T>>& pairs)
+{
+    auto result = pairs | std::ranges::views::values;
+    return std::vector<T>(result.begin(), result.end());
+}
 }
