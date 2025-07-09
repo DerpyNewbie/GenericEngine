f086760a1d52f50892e5937ee5487b56cce2aaa8
DerpyNewbie <contact@derpynewbie.dev>
2025-07-10 04:55:30 +0900

    Add missing gui.h and gui.cpp

---
+++ Engine/gui.cpp
@@ -0,0 +1,170 @@
+#include "pch.h"
+#include "gui.h"
+
+#include "str_util.h"
+#include "Asset/asset_database.h"
+
+#include <shobjidl_core.h>
+
+namespace engine
+{
+bool Gui::OpenFileDialog(std::string &file_path, const std::vector<FilterSpec> &filters)
+{
+    IFileOpenDialog *p_file_open;
+    HRESULT hr = CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL,
+                                  IID_IFileOpenDialog, reinterpret_cast<void **>(&p_file_open));
+
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    if (!filters.empty())
+    {
+        hr = p_file_open->SetFileTypes(filters.size(), filters.data());
+        if (FAILED(hr))
+        {
+            Logger::Error<Gui>("Failed to set file types");
+        }
+    }
+
+    hr = p_file_open->Show(nullptr);
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to show file open dialog");
+        return false;
+    }
+
+    IShellItem *p_item;
+    hr = p_file_open->GetResult(&p_item);
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to get result from shell item");
+        return false;
+    }
+
+    PWSTR path;
+    hr = p_item->GetDisplayName(SIGDN_FILESYSPATH, &path);
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to get display name from shell item");
+        return false;
+    }
+
+    file_path = StringUtil::Utf16ToUtf8(std::wstring(path));
+    CoTaskMemFree(path);
+    return true;
+
+}
+
+bool Gui::SaveFileDialog(std::string &file_path, const std::string &default_name,
+                         const std::vector<FilterSpec> &filters)
+{
+    IFileSaveDialog *p_file_save;
+
+    HRESULT hr = CoCreateInstance(CLSID_FileSaveDialog, nullptr, CLSCTX_ALL,
+                                  IID_IFileSaveDialog, reinterpret_cast<void **>(&p_file_save));
+
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    hr = p_file_save->SetFileName(StringUtil::Utf8ToUtf16(default_name).c_str());
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to set default name");
+    }
+
+    hr = p_file_save->SetFileTypes(filters.size(), filters.data());
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to set file types");
+    }
+
+    hr = p_file_save->Show(nullptr);
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("File save cancelled");
+        return false;
+    }
+
+    IShellItem *p_item;
+    hr = p_file_save->GetResult(&p_item);
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to get result from shell item");
+        return false;
+    }
+
+    PWSTR path;
+    hr = p_item->GetDisplayName(SIGDN_FILESYSPATH, &path);
+    if (FAILED(hr))
+    {
+        Logger::Error<Gui>("Failed to get display name from shell item");
+        return false;
+    }
+
+    file_path = StringUtil::Utf16ToUtf8(std::wstring(path));
+    CoTaskMemFree(path);
+    return true;
+}
+
+void Gui::SetObjectDragDropTarget(const std::shared_ptr<Object> &object)
+{
+    SetObjectDragDropTarget(object->Guid());
+}
+
+void Gui::SetObjectDragDropTarget(const xg::Guid guid)
+{
+    const auto guid_str = guid.str();
+    ImGui::SetDragDropPayload(DragDropTarget::kObjectGuid, guid_str.c_str(), guid_str.size() + 1);
+}
+
+std::shared_ptr<Object> Gui::GetObjectDragDropTarget(const ImGuiPayload *payload)
+{
+    if (payload == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (!payload->IsDataType(DragDropTarget::kObjectGuid))
+    {
+        return nullptr;
+    }
+
+    // search for instantiated objects
+    const auto guid_str = std::string(static_cast<char *>(payload->Data));
+    const auto guid = xg::Guid(guid_str);
+    const auto object = Object::Find(guid);
+
+    if (object != nullptr)
+    {
+        return object;
+    }
+
+    // search for unloaded AssetDatabase assets
+    const auto asset_descriptor = AssetDatabase::GetAssetDescriptor(guid);
+    if (asset_descriptor == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (asset_descriptor->managed_object == nullptr)
+    {
+        asset_descriptor->Reload();
+    }
+
+    if (asset_descriptor->managed_object == nullptr)
+    {
+        return nullptr;
+    }
+
+    return asset_descriptor->managed_object;
+}
+
+std::shared_ptr<Object> Gui::GetObjectDragDropTarget()
+{
+    return GetObjectDragDropTarget(ImGui::GetDragDropPayload());
+}
+}
---
+++ Engine/gui.h
@@ -0,0 +1,218 @@
+#pragma once
+#include "game_object.h"
+#include "inspectable.h"
+#include "Asset/asset_ptr.h"
+
+#include <shtypes.h>
+
+namespace engine
+{
+using FilterSpec = COMDLG_FILTERSPEC;
+using namespace engine;
+
+template <typename T>
+static constexpr bool is_inspectable = false;
+
+template <typename T> requires std::is_same_v<decltype(std::declval<T>().OnInspectorGui()), void>
+static constexpr bool is_inspectable<T> = true;
+
+class Gui
+{
+public:
+    struct DragDropTarget
+    {
+        static constexpr auto kObject = "ENGINE_OBJECT";
+        static constexpr auto kObjectGuid = "ENGINE_OBJ_GUID";
+    };
+
+    static bool OpenFileDialog(std::string &file_path, const std::vector<FilterSpec> &filters = {});
+
+    static bool SaveFileDialog(std::string &file_path, const std::string &default_name,
+                               const std::vector<FilterSpec> &filters = {});
+
+    static void SetObjectDragDropTarget(const std::shared_ptr<Object> &object);
+    static void SetObjectDragDropTarget(xg::Guid guid);
+
+    static std::shared_ptr<Object> GetObjectDragDropTarget(const ImGuiPayload *payload);
+    static std::shared_ptr<Object> GetObjectDragDropTarget();
+
+    template <typename T> requires !std::is_base_of_v<Component, T>
+    static std::shared_ptr<T> MakeCompatible(std::shared_ptr<Object> object);
+
+    template <typename T> requires std::is_base_of_v<Component, T>
+    static std::shared_ptr<T> MakeCompatible(std::shared_ptr<Object> object);
+
+    template <typename T>
+    static bool AssetDragDropTarget(IAssetPtr &asset_ptr);
+
+    template <typename T>
+    static bool AssetPicker(IAssetPtr &asset_ptr);
+
+    template <typename T>
+    static bool PropertyField(const char *label, IAssetPtr &value);
+
+    template <typename T>
+    static bool PropertyField(const char *label, AssetPtr<T> &value)
+    {
+        return PropertyField<T>(label, static_cast<IAssetPtr &>(value));
+    }
+
+    template <typename T> requires is_inspectable<T>
+    static bool ExpandablePropertyField(const char *label, IAssetPtr &value);
+
+    template <typename T> requires is_inspectable<T>
+    static bool ExpandablePropertyField(const char *label, AssetPtr<T> &value)
+    {
+        return ExpandablePropertyField<T>(label, static_cast<IAssetPtr &>(value));
+    }
+};
+
+template <typename T> requires !std::is_base_of_v<Component, T>
+std::shared_ptr<T> Gui::MakeCompatible(const std::shared_ptr<Object> object)
+{
+    return std::dynamic_pointer_cast<T>(object);
+}
+
+template <typename T> requires std::is_base_of_v<Component, T>
+std::shared_ptr<T> Gui::MakeCompatible(const std::shared_ptr<Object> object)
+{
+    auto casted = std::dynamic_pointer_cast<T>(object);
+    if (casted != nullptr)
+        return casted;
+
+    const auto game_object = std::dynamic_pointer_cast<GameObject>(object);
+    if (game_object != nullptr)
+        return game_object->GetComponent<T>();
+
+    return nullptr;
+}
+
+template <typename T>
+bool Gui::AssetDragDropTarget(IAssetPtr &asset_ptr)
+{
+    if (!ImGui::BeginDragDropTarget())
+    {
+        return false;
+    }
+
+    const auto payload = ImGui::GetDragDropPayload();
+    if (!payload->IsDataType(DragDropTarget::kObjectGuid))
+    {
+        ImGui::EndDragDropTarget();
+        return false;
+    }
+
+    const auto object = GetObjectDragDropTarget(payload);
+
+    auto casted_object = MakeCompatible<T>(object);
+    if (casted_object == nullptr)
+    {
+        ImGui::Text("Type mismatch. Requires '%s'", typeid(T).name());
+        ImGui::EndDragDropTarget();
+        return false;
+    }
+
+    if (ImGui::AcceptDragDropPayload(DragDropTarget::kObjectGuid))
+    {
+        asset_ptr = IAssetPtr::FromManaged(casted_object);
+
+        ImGui::EndDragDropTarget();
+        return true;
+    }
+
+    ImGui::EndDragDropTarget();
+    return false;
+}
+
+template <typename T>
+bool Gui::AssetPicker(IAssetPtr &asset_ptr)
+{
+    static_assert(std::is_base_of<Object, T>(), "Base type is not Object.");
+
+    auto objects = Object::FindByType<T>();
+    if (objects.empty())
+    {
+        ImGui::Text("There was no objects with type '%s'", typeid(T).name());
+        return false;
+    }
+
+    if (ImGui::MenuItem("None"))
+    {
+        asset_ptr = IAssetPtr{};
+        ImGui::CloseCurrentPopup();
+        return true;
+    }
+
+    for (auto &object : objects)
+    {
+        auto obj_asset_ptr = IAssetPtr::FromManaged(object);
+        if (ImGui::MenuItem(obj_asset_ptr.Name().c_str()))
+        {
+            asset_ptr = obj_asset_ptr;
+            ImGui::CloseCurrentPopup();
+            return true;
+        }
+
+        if (ImGui::BeginItemTooltip())
+        {
+            ImGui::Text(("GUID: " + object->Guid().str()).c_str());
+            ImGui::EndTooltip();
+        }
+    }
+    return false;
+}
+
+template <typename T>
+bool Gui::PropertyField(const char *label, IAssetPtr &value)
+{
+    static_assert(std::is_base_of<Object, T>(), "Base type is not Object.");
+
+    bool has_changed = false;
+    ImGui::PushID(label);
+    {
+        if (ImGui::BeginPopup("##PROPERTY_FIELD_POPUP"))
+        {
+            has_changed |= AssetPicker<T>(value);
+            ImGui::EndPopup();
+        }
+
+        const auto height = ImGui::GetTextLineHeightWithSpacing();
+        const auto width = ImGui::GetContentRegionAvail().x;
+        if (ImGui::Button(value.Name().c_str(), ImVec2(width * 0.7F - 0.25F, height)))
+        {
+            ImGui::OpenPopup("##PROPERTY_FIELD_POPUP");
+        }
+
+        has_changed |= AssetDragDropTarget<T>(value);
+
+        ImGui::SameLine();
+        ImGui::Text(label);
+    }
+    ImGui::PopID();
+
+    return has_changed;
+}
+
+template <typename T> requires is_inspectable<T>
+bool Gui::ExpandablePropertyField(const char *label, IAssetPtr &value)
+{
+    const bool result = PropertyField<T>(label, value);
+    ImGui::PushID(label);
+    if (ImGui::CollapsingHeader(label))
+    {
+        ImGui::Indent();
+        const auto locked = std::dynamic_pointer_cast<T>(value.Lock());
+        if (locked == nullptr)
+        {
+            ImGui::Text("There is nothing to show...");
+        }
+        else
+        {
+            locked->OnInspectorGui();
+        }
+        ImGui::Unindent();
+    }
+    ImGui::PopID();
+    return result;
+}
+}
