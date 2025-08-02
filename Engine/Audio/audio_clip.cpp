de7736d1b2141ef1dc203aa8873dd6bb1c900a86
DerpyNewbie <contact@derpynewbie.dev>
2025-08-02 22:42:43 +0900

    Add previewing for AudioClip inspector

--- Engine/Audio/audio_clip.cpp
+++ Engine/Audio/audio_clip.cpp
@@ -11,6 +11,33 @@ void AudioClip::OnInspectorGui()
     ImGui::Text("Duration: %s", StringUtil::DurationToString(Duration()).c_str());
     ImGui::Text("Channels: %d", Channels());
     ImGui::Text("Sample Rate: %d", SampleRate());
+
+    static std::unique_ptr<DirectX::SoundEffectInstance> preview_instance;
+    if (preview_instance == nullptr && ImGui::Button("Play"))
+    {
+        preview_instance = m_sound_effect_->CreateInstance();
+        preview_instance->Play();
+    }
+    if (preview_instance != nullptr)
+    {
+        if (ImGui::Button("Stop"))
+        {
+            preview_instance->Stop();
+            preview_instance.reset();
+        }
+
+        ImGui::SameLine();
+        if (ImGui::Button("Pause"))
+        {
+            preview_instance->Pause();
+        }
+
+        ImGui::SameLine();
+        if (ImGui::Button("Resume"))
+        {
+            preview_instance->Resume();
+        }
+    }
 }
 
 std::chrono::milliseconds AudioClip::Duration() const
