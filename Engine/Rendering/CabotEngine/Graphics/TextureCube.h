540a1705893d5fd3eea31d674897ebc43b249632
xiar5 <xiar5@>
2025-08-25 15:35:33 +0900

    不必要そうなところを消した

--- Engine/Rendering/CabotEngine/Graphics/TextureCube.h
+++ Engine/Rendering/CabotEngine/Graphics/TextureCube.h
@@ -1,12 +1,9 @@
 #pragma once
 #include "Texture2D.h"
 
-#include <array>
-#include <directx/d3d12.h>
-
 class TextureCube
 {
-    Microsoft::WRL::ComPtr<ID3D12Resource> m_pResource_;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_p_resource_;
 
 public:
     bool CreateTexCube(const std::array<std::shared_ptr<Texture2D>, 6> &textures);
