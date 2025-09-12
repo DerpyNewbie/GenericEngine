b007032fd39dc89d86d8482d4c75ac63254a925a
DerpyNewbie <contact@derpynewbie.dev>
2025-09-12 23:58:00 +0900

    Add editor attaching and scene creation back online

---
+++ Engine/default_scene_generator.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "default_scene_generator.h"
+
+#include "game_object.h"
+#include "Audio/audio_listener_component.h"
+#include "Components/camera.h"
+#include "Physics/plane_collider.h"
+
+namespace engine
+{
+void SampleSceneGenerator::CreateDefaultScene()
+{
+    CreateDefaultCamera();
+    CreateDefaultFloor();
+}
+
+void SampleSceneGenerator::CreateDefaultCamera()
+{
+    // Sample scene creation
+    const auto camera = Object::Instantiate<GameObject>("Camera");
+    camera->AddComponent<Camera>();
+    camera->AddComponent<AudioListenerComponent>();
+}
+
+void SampleSceneGenerator::CreateDefaultFloor()
+{
+    Object::Instantiate<GameObject>("Floor")->AddComponent<PlaneCollider>();
+}
+}
