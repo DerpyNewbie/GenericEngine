73d72f4c2970fa2f876c82f19069bd6600c67647
xiar5 <xiar5@>
2025-10-21 19:59:59 +0900

    Lightのデータの位置を移動しました

--- Engine/Components/light.h
+++ Engine/Components/light.h
@@ -7,43 +7,6 @@
 
 namespace engine
 {
-struct alignas(16) LightData
-{
-    int type = 1;
-    int cast_shadow = 0;
-    float intensity = 1;
-    float range = 1;
-
-    Vector4 pos;
-    Quaternion direction;
-    Color color;
-
-    float inner_cos;
-    float outer_cos;
-    float padding[2];
-
-    template <class Archive>
-    void serialize(Archive &ar)
-    {
-        ar(CEREAL_NVP(type),
-           CEREAL_NVP(cast_shadow),
-           CEREAL_NVP(intensity),
-           CEREAL_NVP(range),
-           CEREAL_NVP(pos),
-           CEREAL_NVP(direction),
-           CEREAL_NVP(color),
-           CEREAL_NVP(inner_cos),
-           CEREAL_NVP(outer_cos));
-    }
-};
-
-
-struct alignas(256) LightCountBuffer
-{
-    ~LightCountBuffer() = default;
-    uint32_t count;
-};
-
 class Light : public Component
 {
     friend class CameraComponent;
