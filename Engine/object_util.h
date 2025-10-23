2cc1e91cea3096cd17dd4d143e8f2df5c395cc4f
DerpyNewbie <contact@derpynewbie.dev>
2025-10-23 20:25:03 +0900

    Remove unused piece of code

--- Engine/object_util.h
+++ Engine/object_util.h
@@ -2,66 +2,6 @@
 
 namespace engine
 {
-// class SerializedObjectProxy
-// {
-//     rapidjson::Document::Object &m_json_object_;
-//
-// public:
-//     SerializedObjectProxy(rapidjson::Document::Object &obj) : m_json_object_(obj)
-//     {}
-//
-//     xg::Guid GetGuid() const
-//     {
-//         return xg::Guid(m_json_object_.FindMember("m_guid_")->value.GetString());
-//     }
-//
-//     void SetGuid(xg::Guid guid) const
-//     {
-//         auto guid_str = guid.str();
-//         m_json_object_.FindMember("m_guid_")->value.SetString(guid_str.c_str(), guid_str.size());
-//     }
-//
-//     std::string GetName() const
-//     {
-//         return m_json_object_.FindMember("m_name_")->value.GetString();
-//     }
-//
-//     void SetName(const std::string &name) const
-//     {
-//         m_json_object_.FindMember("m_name_")->value.SetString(name.c_str(), name.size());
-//     }
-// };
-//
-// class SerializedAssetPtrProxy
-// {
-//     rapidjson::Document::Object &m_json_object_;
-//
-// public:
-//     SerializedAssetPtrProxy(rapidjson::Document::Object &obj) : m_json_object_(obj)
-//     {}
-//
-//     xg::Guid GetGuid() const
-//     {
-//         return xg::Guid(m_json_object_.FindMember("m_guid_")->value.GetString());
-//     }
-//
-//     void SetGuid(xg::Guid guid) const
-//     {
-//         auto guid_str = guid.str();
-//         m_json_object_.FindMember("m_guid_")->value.SetString(guid_str.c_str(), guid_str.size());
-//     }
-//
-//     AssetPtrType GetAssetPtrType() const
-//     {
-//         return static_cast<AssetPtrType>(m_json_object_.FindMember("m_type_")->value.GetInt());
-//     }
-//
-//     void SetAssetPtrType(AssetPtrType type) const
-//     {
-//         m_json_object_.FindMember("m_type_")->value.SetInt(static_cast<int>(type));
-//     }
-// };
-
 class ObjectUtil
 {
 public:
@@ -72,7 +12,7 @@ public:
     static bool IsAssetPtr(const rapidjson::Document::Object &object);
     static std::vector<rapidjson::Document::Object> FindMatchingObjects(const rapidjson::Document::Object &object,
                                                                         const std::function<bool(
-                                                                            rapidjson::Document::Object)> &pred);
+                                                                        rapidjson::Document::Object)> &pred);
 
 };
 }
