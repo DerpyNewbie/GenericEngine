3a3964366fc804e2c76d5fbf83798ff414a6ad40
DerpyNewbie <contact@derpynewbie.dev>
2025-11-29 02:28:25 +0900

    Make m_deserialized_object store weak ptr instead of shared ptr

--- Engine/object.cpp
+++ Engine/object.cpp
@@ -11,13 +11,6 @@ std::vector<std::shared_ptr<engine::Object>> g_destroyed_objects;
 
 namespace engine
 {
-
-bool Object::m_in_gc_time_ = false;
-unsigned int Object::m_last_instantiated_name_count_ = 0;
-unsigned int Object::m_last_immediately_destroyed_objects_ = 0;
-std::unordered_map<xg::Guid, std::shared_ptr<Object>> Object::m_objects_;
-std::vector<std::shared_ptr<Object>> Object::m_deserialized_objects_;
-
 void Object::GarbageCollect()
 {
     m_in_gc_time_ = true;
@@ -51,9 +44,12 @@ void Object::GarbageCollect()
 void Object::InvokeOnDeserialized()
 {
     const auto deserialized_objects = m_deserialized_objects_;
-    for (const auto &obj : deserialized_objects)
+    for (const auto &weak_obj : deserialized_objects)
     {
-        obj->OnDeserialized();
+        if (const auto obj = weak_obj.lock())
+        {
+            obj->OnDeserialized();
+        }
     }
 
     m_deserialized_objects_.clear();
@@ -128,8 +124,10 @@ void Object::DestroyImmediate(const std::shared_ptr<Object> &obj)
 
     if (m_in_gc_time_)
     {
-        Logger::Warn<Object>("Cannot immediately destroy object `%s` during GC cycle. Use Object::Destroy instead.",
-                             obj->Name().c_str());
+        Logger::Warn<Object>(
+            "Cannot immediately destroy object `%s` during GC cycle. Use Object::Destroy instead.",
+            obj->Name().c_str()
+        );
         Destroy(obj);
         return;
     }
@@ -138,7 +136,8 @@ void Object::DestroyImmediate(const std::shared_ptr<Object> &obj)
     {
         Logger::Warn<Object>(
             "Cannot immediately destroy object `%s` during UpdateManager cycle. Use Object::Destroy instead.",
-            obj->Name().c_str());
+            obj->Name().c_str()
+        );
         Destroy(obj);
         return;
     }
