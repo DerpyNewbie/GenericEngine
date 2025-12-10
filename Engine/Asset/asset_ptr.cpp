1932eeea37c0403f2a76d72b01b828025d2d068a
DerpyNewbie <contact@derpynewbie.dev>
2025-12-10 15:40:19 +0900

    Fix "(Missing)" display after scene deserialization caused by caching

--- Engine/Asset/asset_ptr.cpp
+++ Engine/Asset/asset_ptr.cpp
@@ -55,58 +55,71 @@ std::shared_ptr<Object> IAssetPtr::Lock()
 {
     switch (m_type_)
     {
-    default:
-    case AssetPtrType::kNull:
-        return nullptr;
-    case AssetPtrType::kStoredReference:
-        return m_stored_reference_;
-    case AssetPtrType::kExternalReference: {
-        const auto lock = m_external_reference_.lock();
-        if (lock == nullptr)
-        {
-            auto obj = Object::Find(m_guid_);
-            if (obj != nullptr)
-            {
-                m_external_reference_ = obj;
-                return obj;
-            }
-
-            const auto asset_descriptor = AssetDatabase::GetAssetDescriptor(m_guid_);
-            if (asset_descriptor != nullptr)
-            {
-                asset_descriptor->Import();
-                m_external_reference_ = asset_descriptor->MainObject();
-                return asset_descriptor->MainObject();
-            }
-
-            // missing reference
+        default:
+        case AssetPtrType::kNull:
             return nullptr;
-        }
+        case AssetPtrType::kStoredReference:
+            return m_stored_reference_;
+        case AssetPtrType::kExternalReference: {
+            const auto lock = m_external_reference_.lock();
+            if (lock == nullptr)
+            {
+                auto obj = Object::Find(m_guid_);
+                if (obj != nullptr)
+                {
+                    m_external_reference_ = obj;
+                    return obj;
+                }
 
-        return lock;
-    }
+                const auto asset_descriptor = AssetDatabase::GetAssetDescriptor(m_guid_);
+                if (asset_descriptor != nullptr)
+                {
+                    asset_descriptor->Import();
+                    m_external_reference_ = asset_descriptor->MainObject();
+                    return asset_descriptor->MainObject();
+                }
+
+                // missing reference
+                return nullptr;
+            }
+
+            return lock;
+        }
     }
 }
 
 std::string IAssetPtr::Name() const
 {
-    const auto locked = m_external_reference_.lock();
-    if (locked != nullptr)
+    switch (m_type_)
     {
-        const auto component = std::dynamic_pointer_cast<Component>(locked);
-        if (component != nullptr && component->GameObject() != nullptr)
-        {
-            return component->GameObject()->Name() + " (" + component->Name() + ")";
+        case AssetPtrType::kNull: {
+            return "(None)";
         }
-        return locked->Name();
-    }
+        case AssetPtrType::kStoredReference: {
+            if (m_stored_reference_ == nullptr)
+                return "(Null)";
+            return m_stored_reference_->Name();
+        }
+        case AssetPtrType::kExternalReference: {
+            const auto referencing_object = Object::Find(m_guid_);
+            if (referencing_object == nullptr)
+            {
+                return "(Missing)";
+            }
 
-    if (IsNull())
-    {
-        return "(None)";
-    }
+            // component will retrieve its attached game object's name. e.x. "BarGo (FooComponent)"
+            const auto component = std::dynamic_pointer_cast<Component>(referencing_object);
+            if (component != nullptr && component->GameObject() != nullptr)
+            {
+                return component->GameObject()->Name() + " (" + component->Name() + ")";
+            }
 
-    return "(Missing)";
+            return referencing_object->Name();
+        }
+        default: {
+            return "(Unknown)";
+        }
+    }
 }
 bool IAssetPtr::IsNull() const
 {
