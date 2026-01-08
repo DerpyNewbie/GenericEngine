1a46b5cc93d5ed4f5d8e26f0cae77db07aad1330
DerpyNewbie <contact@derpynewbie.dev>
2026-01-08 10:18:15 +0900

    Add MeshCollider impl

--- Engine/Physics/mesh_collider.h
+++ Engine/Physics/mesh_collider.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <BulletCollision/CollisionShapes/btBvhTriangleMeshShape.h>
+#include <BulletCollision/CollisionShapes/btConvexHullShape.h>
+#include <BulletCollision/CollisionShapes/btTriangleMesh.h>
+
 #include "collider.h"
 #include "Asset/asset_ptr.h"
 #include "Rendering/mesh.h"
@@ -8,8 +12,27 @@ namespace engine
 class MeshCollider : public Collider
 {
     AssetPtr<Mesh> m_mesh_;
+    bool m_use_convex_ = false;
 
+    std::shared_ptr<btTriangleIndexVertexArray> m_tri_mesh_;
+    std::shared_ptr<btBvhTriangleMeshShape> m_bvh_tri_shape_;
+    std::shared_ptr<btConvexHullShape> m_convex_hull_shape_;
+
+    void OnInspectorGui() override;
+    void OnConstructed() override;
+
+    std::shared_ptr<btCollisionShape> GetShape() override;
     void UpdateShape() override;
-    btCollisionShape *GetShape() override;
+
+public:
+    void SetMesh(const AssetPtr<Mesh> &mesh);
+
+    template <class Archive>
+    void serialize(Archive &ar, const uint32_t version)
+    {
+        ar(cereal::base_class<Collider>(this), CEREAL_NVP(m_mesh_), CEREAL_NVP(m_use_convex_));
+    }
 };
 }
+
+CEREAL_CLASS_VERSION(engine::MeshCollider, 1)
