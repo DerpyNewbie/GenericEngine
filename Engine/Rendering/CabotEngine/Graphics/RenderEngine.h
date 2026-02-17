20f63c501c3f00ca472e10e5ecda1f450eacfd52
xiar5 <xiar5@>
2026-02-17 19:49:31 +0900

    WaitRenderを消したよ

--- Engine/Rendering/CabotEngine/Graphics/RenderEngine.h
+++ Engine/Rendering/CabotEngine/Graphics/RenderEngine.h
@@ -65,7 +65,6 @@ public:
         Color background_color, const D3D12_VIEWPORT *viewport = nullptr, const D3D12_RECT *scissor = nullptr) const;
     void EndRender();
     void MoveToNextFrame();
-    void WaitRender();
     void UpdateMainRenderTarget();
 
     static ID3D12Device6 *Device()
