e5312a60eb2e25642c40fd2cd5366f8354314568
xiar5 <xiar5@>
2026-04-27 23:35:27 +0900

    IndexBufferをリファクタリングしました

--- Engine/Rendering/CabotEngine/Graphics/IndexBuffer.cpp
+++ Engine/Rendering/CabotEngine/Graphics/IndexBuffer.cpp
@@ -3,58 +3,80 @@
 #include "IndexBuffer.h"
 #include "RenderEngine.h"
 
-engine::IndexBuffer::IndexBuffer(size_t size, const uint32_t *pInitData)
+engine::IndexBuffer::IndexBuffer(size_t size, const uint32_t *init_data)
 {
-    auto prop = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD); // ヒーププロパティ
-    D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(size); // リソースの設定
+    if (init_data == nullptr)
+    {
+        Logger::Error<IndexBuffer>("index buffer initialization data is null");
+        return;
+    }
 
-    // Create resource
+    auto upload_prop = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+    D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(size);
+
     auto hr = RenderEngine::Device()->CreateCommittedResource(
-        &prop,
+        &upload_prop,
         D3D12_HEAP_FLAG_NONE,
         &desc,
         D3D12_RESOURCE_STATE_GENERIC_READ,
         nullptr,
-        IID_PPV_ARGS(m_pBuffer.GetAddressOf()));
+        IID_PPV_ARGS(m_upload_resource_.GetAddressOf()));
+
     if (FAILED(hr))
     {
-        Logger::Error<IndexBuffer>("failed to create index buffer resource");
+        Logger::Error<IndexBuffer>("failed to create index buffer upload resource");
         return;
     }
 
-    // Setup index buffer view
-    m_View = {};
-    m_View.BufferLocation = m_pBuffer->GetGPUVirtualAddress();
-    m_View.Format = DXGI_FORMAT_R32_UINT;
-    m_View.SizeInBytes = static_cast<UINT>(size);
+    void *ptr = nullptr;
 
-    // Create mapping
-    if (pInitData != nullptr)
+    constexpr D3D12_RANGE read_range = {0, 0};
+    hr = m_upload_resource_->Map(0, &read_range, &ptr);
+    if (FAILED(hr))
     {
-        void *ptr = nullptr;
-        hr = m_pBuffer->Map(0, nullptr, &ptr);
-        if (FAILED(hr))
-        {
-            Logger::Error<IndexBuffer>("failed to index buffer mapping");
-            return;
-        }
-
-        // Copy indices data to mapped ptr
-        memcpy(ptr, pInitData, size);
-
-        // Clean up mapping
-        m_pBuffer->Unmap(0, nullptr);
+        Logger::Error<IndexBuffer>("failed to index buffer mapping");
+        m_upload_resource_.Reset();
+        return;
     }
-    m_pBuffer->SetName(L"IndexBuffer");
-    m_IsValid = true;
+
+    memcpy(ptr, init_data, size);
+
+    m_upload_resource_->Unmap(0, nullptr);
+
+    const auto default_prop = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+    hr = RenderEngine::Device()->CreateCommittedResource(
+        &default_prop,
+        D3D12_HEAP_FLAG_NONE,
+        &desc,
+        D3D12_RESOURCE_STATE_COPY_DEST,
+        nullptr,
+        IID_PPV_ARGS(m_default_resource_.GetAddressOf()));
+
+    if (FAILED(hr))
+    {
+        Logger::Error<IndexBuffer>("failed to create index buffer default resource");
+        m_default_resource_.Reset();
+        return;
+    }
+
+    RenderEngine::CommandList()->CopyBufferRegion(m_default_resource_.Get(), 0, m_upload_resource_.Get(), 0, size);
+
+    auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_default_resource_.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_INDEX_BUFFER);
+    RenderEngine::CommandList()->ResourceBarrier(1, &barrier);
+
+    m_view_.BufferLocation = m_default_resource_->GetGPUVirtualAddress();
+    m_view_.Format = DXGI_FORMAT_R32_UINT;
+    m_view_.SizeInBytes = static_cast<UINT>(size);
+
+    m_default_resource_->SetName(L"IndexBuffer");
 }
 
 bool engine::IndexBuffer::IsValid() const
 {
-    return m_IsValid;
+    return m_default_resource_ != nullptr;
 }
 
 D3D12_INDEX_BUFFER_VIEW *engine::IndexBuffer::View()
 {
-    return &m_View;
+    return &m_view_;
 }
--- Engine/Rendering/CabotEngine/Graphics/IndexBuffer.h
+++ Engine/Rendering/CabotEngine/Graphics/IndexBuffer.h
@@ -3,17 +3,18 @@ namespace engine
 {
 class IndexBuffer
 {
+    //TODO : upload_resourceを保持する必要があるか？初期化後は不要なはず、ExecuteCommandListの後にResetしましょう
+    ComPtr<ID3D12Resource> m_upload_resource_;
+    ComPtr<ID3D12Resource> m_default_resource_;
+    D3D12_INDEX_BUFFER_VIEW m_view_;
+    
 public:
-    IndexBuffer(size_t size, const uint32_t *pInitData = nullptr);
-    bool IsValid() const;
-    D3D12_INDEX_BUFFER_VIEW *View();
-
-private:
-    bool m_IsValid = false;
-    ComPtr<ID3D12Resource> m_pBuffer; // インデックスバッファ
-    D3D12_INDEX_BUFFER_VIEW m_View; // インデックスバッファビュー
-
+    explicit IndexBuffer(size_t size, const uint32_t *init_data = nullptr);
     IndexBuffer(const IndexBuffer &) = delete;
     void operator =(const IndexBuffer &) = delete;
+
+    [[nodiscard]] bool IsValid() const;
+    D3D12_INDEX_BUFFER_VIEW *View();
+
 };
 }
